2f1bb6a693a2537b488db991107d39c62c02d000
theprover97 <theprover97@gmail.com>
2019-04-22 19:50:33 +0530

    Update models.py

--- chat_app/models.py
+++ chat_app/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Post(models.Model):
+    user_name = models.CharField(max_length=255)
+    #slug = models.SlugField(unique=True, max_length=255)
+    password = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    
