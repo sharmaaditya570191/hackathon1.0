07755baa24a9086d8370410831082121027ee8f7
theprover97 <theprover97@gmail.com>
2019-04-22 18:23:17 +0530

    Update urls.py

--- hackathon1/urls.py
+++ hackathon1/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+from chat_app import views
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('', views.index, name='index'),
 ]
