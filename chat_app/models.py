3fbb57158116c5d27da9226c16aba3786552229d
Dantraz Biyean Trevizion Tremik <ayushpratap16@gmail.com>
2019-04-22 20:00:40 +0530

    Update models.py

--- chat_app/models.py
+++ chat_app/models.py
@@ -2,7 +2,6 @@ from django.db import models
 
 class Post(models.Model):
     user_name = models.CharField(max_length=255)
-    #slug = models.SlugField(unique=True, max_length=255)
     password = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
-    
+    message=models.TextField()
