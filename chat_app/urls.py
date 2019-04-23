59d0604405258425a591cbcd30d3f3b03085dd8d
Dantraz Biyean Trevizion Tremik <ayushpratap16@gmail.com>
2019-04-23 19:47:47 +0530

    register page

--- chat_app/urls.py
+++ chat_app/urls.py
@@ -2,4 +2,6 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('home/', views.post_list, name='post_list'),
+    path('signup/', views.SignUp.as_view(), name='signup'),
+
 ]
--- hackathon1/urls.py
+++ hackathon1/urls.py
@@ -16,9 +16,13 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from chat_app import views
+from django.views.generic.base import TemplateView
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('', include('chat_app.urls')),
+    path('home/', views.index, name='index'),
+    path('home2/', include('chat_app.urls')),
     path('admin/', admin.site.urls),
-    path('accounts/', include('django.contrib.auth.urls')),
+    path('acc/', include('django.contrib.auth.urls')),
+    path('', TemplateView.as_view(template_name='home.html'), name='home'),
+    path('acc/', include('chat_app.urls')), # new
+
     ]
