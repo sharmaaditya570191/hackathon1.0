c5a7deeb7f1ad4c355eaf3e7e11d3ca5f6e5a064
Aditya Sharma <42587159+sharmaaditya570191@users.noreply.github.com>
2019-04-23 15:40:25 +0530

    Overriding default primary key

--- chat_app/models.py
+++ chat_app/models.py
@@ -2,7 +2,7 @@ from django.conf import settings
 from django.db import models
 
 class Post(models.Model):
-    user_name = models.CharField(max_length=255)
+    user_name = models.CharField(max_length=255,primary_key=True)
     password = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
     message=models.TextField()
